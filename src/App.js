a4478f2a41c82aa7a23a29ae85e43090288a2b30
Alex Zhao <alexzhao21@gmail.com>
2018-03-31 02:27:51 -0700

    Add router skeleton

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+
+
+        <Route exact path='/' />
+
+
+        </div>
+      </Router>
     );
   }
 }
 
 export default App;
+
+/*
+          <ul>
+            <li><Link to="/">Login</Link></li>
+            <li><Link to="/home">Home</Link></li>
+            <Button primary> Test </Button>
+          </ul>
+
+
+                    <Route exact path="/" component={Login}/>
+          <Route path="/home" component={Home}/>
+*/
