23f4eeffff0c2ec4cc31adaedc5d4f706c680b0c
ItelSunday <simplyitel@gmail.com>
2018-03-31 14:42:43 -0700

    added buttons

--- src/App.js
+++ src/App.js
@@ -1,47 +1,16 @@
 import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-import Landing from './containers/Landing/Landing.js';
+import Landing from './Components/Landing/Landing';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-
-
-        <Route exact path='/' component={Landing}/>
-
-
-        </div>
-      </Router>
+      <div>
+        <Landing />
+      </div>
     );
   }
 }
 
 export default App;
-
-/*
-          <ul>
-            <li><Link to="/">Login</Link></li>
-            <li><Link to="/home">Home</Link></li>
-            <Button primary> Test </Button>
-          </ul>
-
-
-                    <Route exact path="/" component={Login}/>
-          <Route path="/home" component={Home}/>
-*/
