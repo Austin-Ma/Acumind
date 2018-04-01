43693ee476aec50cca661a2c35f488b9da38903e
ItelSunday <simplyitel@gmail.com>
2018-04-01 10:22:16 -0700

    updated navbar and jumbotron

--- src/components/AcuNav/AcuNav.js
+++ src/components/AcuNav/AcuNav.js
@@ -12,19 +12,27 @@ class AcuNav extends Component {
   render() {
     return (
       <Navbar className="navbar" expand="md">
-      <NavbarBrand>Acumind</NavbarBrand>
-      <Nav className="mx-auto logo" navbar>
-        <NavItem>
-          <img className="logo" src={logo} alt="Twitter" />
-        </NavItem>
-      </Nav>
-      <Nav className="ml-md-auto" navbar>
-        <NavItem>
-          {(this.props.isResultsPage) ? <Button outline color="danger" onClick={this.props.onClick}>Log Out</Button> : <Button outline color="info" onClick={this.props.onClick}>Get Started</Button>}
-        </NavItem>
-      </Nav>
-    </Navbar>
-    )
+        <NavbarBrand>Acumind</NavbarBrand>
+        <Nav className="mx-auto logo" navbar>
+          <NavItem>
+            <img className="logo" src={logo} alt="Twitter" />
+          </NavItem>
+        </Nav>
+        <Nav className="ml-md-auto" navbar>
+          <NavItem>
+            {this.props.isResultsPage ? (
+              <Button outline color="primary" onClick={this.props.onClick}>
+                Log Out
+              </Button>
+            ) : (
+              <Button outline color="info" onClick={this.props.onClick}>
+                Get Started
+              </Button>
+            )}
+          </NavItem>
+        </Nav>
+      </Navbar>
+    );
   }
 }
 
--- src/containers/Results/Results.js
+++ src/containers/Results/Results.js
@@ -1,41 +1,83 @@
 import React, { Component } from 'react';
-import { Button } from 'reactstrap';
+import { Button, Container, Jumbotron, Row, Col } from 'reactstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import axios from 'axios';
 import './Results.css';
 
 import LoadingModal from '../../components/LoadingModal/LoadingModal.js';
+import AcuNav from '../../components/AcuNav/AcuNav.js';
+import Auth from '../../components/Auth/Auth.js';
+import NewCard from '../../components/NewCard/NewCard.js';
 
 class Results extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoading: false // handles the displaying of the modal
+      onDisplay: 0, // handles what's being displayed
+      isLoading: true // handles the displaying of the modal
     };
 
-    axios.get("localhost:3000/getData/" + localStorage.getItem("twitter_accesstoken"))
+    axios
+      .get(
+        'https://us-central1-acumind-f0e34.cloudfunctions.net/getDataAlt?userID=' +
+          'orange' /*+ localStorage.getItem("user_id")*/
+      )
       .then(res => {
+        console.log(res.data);
         this.setState({
           data: res.data,
-          isLoading: true
+          isLoading: false
         });
-      }).catch(error => {
-        console.log(error);
       })
+      .catch(error => {
+        console.log(error);
+        // stuck forever in a loading screen :(
+      });
+  }
+
+  logout() {
+    const auth = new Auth();
+    auth.logout();
   }
 
   render() {
     return (
-      <div className="results">
-        <h1>[RESULT]</h1>
-        <h2>{this.state.userID}</h2>
-        <div className="results-buttons">
-          <Button color="primary">Personality</Button>
-          <Button color="primary">Sentiment Analysis</Button>
-          <Button color="primary">Keywords</Button>
-        </div>
-        <LoadingModal className="loading-modal" isOpen={this.state.isLoading} />
+      <div>
+        <AcuNav
+          isResultsPage={true}
+          onClick={() => {
+            this.logout();
+          }}
+        />
+
+        <Jumbotron fluid className="results-jumbotron">
+          <h3 className="results">Results</h3>
+        </Jumbotron>
+
+        <Container>
+          <Row>
+            <Col xs="4">
+              <NewCard
+                cardImgSrc="https://i.imgur.com/U024Hop.png"
+                cardTitle="Personality Traits"
+              />
+            </Col>
+            <Col xs="4">
+              <NewCard
+                cardImgSrc="https://i.imgur.com/u12hID9.png"
+                cardTitle="Sentiment Analysis"
+              />
+            </Col>
+            <Col xs="4">
+              <NewCard
+                cardImgSrc="https://i.imgur.com/EYEzCsa.png"
+                cardTitle="Time-of-day Analysis"
+              />
+            </Col>
+          </Row>
+        </Container>
       </div>
-    )
+    );
   }
 }
 
