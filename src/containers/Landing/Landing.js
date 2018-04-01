124b3582da8f0a3923e93993d661c2923cb73de9
ItelSunday <simplyitel@gmail.com>
2018-03-31 17:43:33 -0700

    added margins and button

--- src/containers/Landing/Landing.js
+++ src/containers/Landing/Landing.js
@@ -1,14 +1,6 @@
 import React, { Component } from 'react';
-
-import {
-  Card,
-  CardImg,
-  CardText,
-  CardBody,
-  CardTitle,
-  CardSubtitle,
-  Button
-} from 'reactstrap';
+import { Container } from 'reactstrap';
+import { Button } from 'reactstrap';
 
 import twitterLogo from '../../assets/twitter.svg';
 
@@ -18,25 +10,28 @@ class Landing extends Component {
   render() {
     return (
       <div>
-        <img className="twitter" src={twitterLogo} align="right" alt="" />
-        <h1>Accumind</h1>
-        <Card>
-          <CardImg
-            top
-            width="70%"
-            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-            alt="Card"
-          />
-          <CardBody>
-            <CardTitle>Card title</CardTitle>
-            <CardSubtitle>Card subtitle</CardSubtitle>
-            <CardText>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </CardText>
-            <Button color="primary">Login to Twitter</Button>
-          </CardBody>
-        </Card>
+        <div className="social-media-container clearfix">
+          <img className="twitter" src={twitterLogo} alt="" />
+        </div>
+
+        <div className="main-title-container">
+          <h1 className="main-title text-center ">Accumind</h1>
+        </div>
+
+        <div>
+          <Container fluid>
+            <div className="description">
+              <p className="lead">
+                A chat visualizer generating data visualization and gives mental
+                health results.
+              </p>
+            </div>
+          </Container>
+        </div>
+
+        <div className="button-container">
+          <Button color="primary">Login to Twitter</Button>
+        </div>
       </div>
     );
   }
